b8272cd85b681ac71c25a63e7f77fd9d4154ceb4
timdahlmann <17274214+timdahlmann@users.noreply.github.com>
2022-06-08 16:51:38 +0200

    Update collapse_reads.pl

--- collapse_reads.pl
+++ collapse_reads.pl
@@ -31,7 +31,7 @@ perl collapse_reads.pl input.fa Cn1 output.fa
 # check if we have the correct number of arguments
 my $num_args = $#ARGV + 1;
 if ($num_args != 3) {
-  print "\nUsage: perl collapse_reads.pl input.fa Cn1 output.fa\n";
+  print "\nUsage: perl collapse_reads.pl input.fa prefix output.fa\n";
   exit;
 }
 
